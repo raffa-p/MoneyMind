6894cb50878ff251ec04b41fa3d2e98791367f20
Raffaele Prota <95775115+raffa-p@users.noreply.github.com>
2025-01-20 17:33:49 +0100

    Add files via upload

---
+++ Server/test/java/com/prota/MoneyMindServer/MoneyMindServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prota.MoneyMindServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMindServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
